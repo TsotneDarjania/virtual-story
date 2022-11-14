78ebb90b5961b4acfc843c2f240cf2b2e419570e
tsotne123 <tsotne@profairgames.com>
2022-11-14 22:50:27 +0400

    add footer

--- src/Components/footer/index.jsx
+++ src/Components/footer/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import company_icon from "../../img/company-icon.png"
+
+import { FaEnvelope } from "react-icons/fa";
+
 
 import "./style.css"
 
 const Footer = () => {
   return (
     <div className='footer'>
-
-
+       <img className='company-icon' src={company_icon} alt="" />
+       <p className='company-name'> Virtual Story 2022</p>
+       <p className='gmail-text'> virtualstory.info@gmail.com</p>
+       <div className='gmail-icon'>
+        <FaEnvelope color='white'  size={50} />
+       </div>
+       
     </div>
   )
 }
